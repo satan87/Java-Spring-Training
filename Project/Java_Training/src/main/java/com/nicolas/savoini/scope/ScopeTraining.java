a5b3a336d6275e4b535a4c5364a8438dceb9f74b
nicolas74 <nicolas.savoini@mac.com>
2022-11-13 19:57:39 -0500

    Create ScopeTraining.java

---
+++ Project/Java_Training/src/main/java/com/nicolas/savoini/scope/ScopeTraining.java
@@ -0,0 +1,24 @@
+package com.nicolas.savoini.scope;
+
+public class ScopeTraining {
+
+  private String classVariable = "This can be used every where in the class";
+
+  public void myFunction(String parameter) {
+
+    String functionVariable = "This can be used only in the function myFunction ";
+
+    String p = parameter; // Parameter has a function scope, it can be used only in the function
+
+  }
+
+
+  public void myOtherFunction() {
+
+    for (int i = 0; i < 5; i++) {
+      String localScope = String.valueOf(i);  // localScope, and i can only be used inside the for loop.
+    }
+    
+  }
+
+}
