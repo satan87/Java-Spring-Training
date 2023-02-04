5b0f8e3b9c394c7817e27ecdef76ed1859240fb3
nicolas74 <nicolas.savoini@mac.com>
2023-02-04 12:24:10 -0500

    Add Test for Record

---
+++ Project/Java_Training/Module-2-c-Record/src/test/java/com/nicolas/savoini/RecordTest.java
@@ -0,0 +1,33 @@
+package com.nicolas.savoini;
+
+import org.junit.jupiter.api.Test;
+
+class RecordTest {
+
+    @Test
+    void testCreateRecordAndPrintIt() {
+
+        // Create a record for a car with an id (int) and a brand (String)
+
+        // Create three different car
+        // Use Atomic Integer to generate next Id
+
+        // Display them on console
+
+    }
+
+    @Test
+    void testCreateRecordAndAddThemTolist() {
+
+        // Create a record for an APi Response (code: Int, success: boolean, message: String)
+        // Create Record for HTTP code 200, 202, 404, and 500
+
+        // Add them to a List
+
+        // Display the list
+
+        // Display only the success codes from the list (Map + Filter + forEach)
+
+    }
+
+}
